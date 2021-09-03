dc088f5dbfc101f1109dc9624b27529080a1fe9a
ys313 <ys3135@gmail.com>
2021-09-03 17:21:00 +0900

    간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,8 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
+
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1(){
@@ -54,7 +58,7 @@ public class OrderSimpleApiController {
         return result;
     }
 
-    @GetMapping("/api/v2/simple-orders")
+    @GetMapping("/api/v3/simple-orders")
     public List<SimpleOrderDto> orderV3(){
         List<Order> orders = orderRepository.findAllWithMemberDelivery();
         List<SimpleOrderDto> result = orders.stream()
@@ -63,19 +67,22 @@ public class OrderSimpleApiController {
 
         return result;
     }
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> orderV4(){
 
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
 
 
     @Data
-    static class SimpleOrderDto{
+    static class SimpleOrderDto {
         private Long orderId;
-        private String name; //Lazy 초기화 :  영속성 컨텍스트가 멤버 아이디를 가지고 영속성 컨텍스트를 찾아본다 없으면 조회
+        private String name;
         private LocalDateTime orderDate;
         private OrderStatus orderStatus;
-        private Address address; ; //Lazy 초기화
+        private Address address;
 
-
-        public SimpleOrderDto(Order order) {
+        public SimpleOrderDto(Order order){
             orderId = order.getId();
             name = order.getMember().getName();
             orderDate = order.getOrderDate();
@@ -83,6 +90,7 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress();
         }
     }
+
 
 
 
